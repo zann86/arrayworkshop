6554b100d75e0765481ba8956a91c34342bbd3cb
suzanna <zannacullen@gmail.com>
2024-01-15 14:54:52 +0000

    Hello World

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 console.log ("Hello World")
+const blogPosts = [
+    "My first blog post",
+    "My second blog post",
+    "My third blog post",
+
+]; console.log (blogPosts);
+
+console.log (blogPosts [0]);
+
+blogPosts [3] = "My fourth blog post";
+console.log (blogPosts[3]);
